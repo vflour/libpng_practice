1c82ebe5d5e3eb23dfd1eab4479f7c9ebfe34cf2
Laz S <vflour@email.com>
2023-02-18 12:25:43 -0500

    Add png writing routines + cleanup

--- png_reader.c
+++ png_reader.c
@@ -8,6 +8,7 @@
 
 #define PIXEL_SIZE 8
 
+
 /// @brief Checks the png signature and outputs to consule
 /// @param f_ptr 
 /// @return 1 if successful
@@ -24,73 +25,13 @@ int check_file(FILE* f_ptr){
     return 1;
 }   
 
-/// @brief Custom callback function for handling unknown chunks. Just prints a message
-/// @param png_ptr 
-/// @param chunk 
-/// @return 
-int chunk_callback(png_structp png_ptr, png_unknown_chunkp chunk){
-    printf("Encountered unknown chunk\n");
-    /**
-     * If you call the png_set_read_user_chunk_fn() function, then all unknown 
-     * chunks which the callback does not handle will be saved when read. 
-     * You can cause them to be discarded by returning '1' ("handled") instead of '0'.
-     */
-    return 1;
-}
-
-
-/// @brief Initialize the two pointer variables for the png. Output to console on failure
-/// @param png_ptr 
-/// @param info_ptr 
-/// @return 1 if successful
-int setup_png(png_structp* png_ptr, png_infop* info_ptr){
-    // Update the png_ptr pointer to the new struct
-    // Meaning you need to dereference the ptr variable and not the ptr itself
-    *png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL,
-      NULL);
-
-    // If there's no pointer then there's an issue
-    if (!png_ptr){
-        printf("Out of memory. \n");
-        return 0;
-    }
-  
-    // Same applies here with the info struct
-    *info_ptr = png_create_info_struct(*png_ptr);
-    if (!info_ptr) {
-        png_destroy_read_struct(png_ptr, NULL, NULL);
-        printf("Out of memory. \n");
-    }
-
-    return 1;
-}
-
-/// @brief Read the background image and init the r,g,b values
-/// @param red 
-/// @param green 
-/// @param blue 
-/// @param png_ptr 
-/// @param info_ptr 
-/// @return 
-int read_bg(uchar* red,uchar* green,uchar* blue, png_structp png_ptr, png_infop info_ptr){
-    if (!png_get_valid(png_ptr, info_ptr, PNG_INFO_bKGD))
-        return 1;
-        
-    // Get pointer to background
-    png_color_16p pBackground;
-    png_get_bKGD(png_ptr, info_ptr, &pBackground);
-
-}
-
 
 /// @brief Reads from the PNG struct
 /// @param png_ptr 
-/// @param info_ptr 
-/// @param f_ptr 
+/// @param info_ptr
+/// @param image
 /// @return 
-int read_png(png_structp png_ptr, png_infop info_ptr, FILE* f_ptr, png_img* image){
-    // According to the guide, it takes the file pointer and stores it in the png pointer
-    png_init_io(png_ptr, f_ptr);
+int read_png(png_structp png_ptr, png_infop info_ptr, png_img* image){
     // Lets the library know that the first 8 bytes were checked, so it wont find it at the file pointer location
     png_set_sig_bytes(png_ptr, 8);
 
@@ -142,27 +83,109 @@ int read_png(png_structp png_ptr, png_infop info_ptr, FILE* f_ptr, png_img* imag
     return 0;
 }
 
-int open_png(FILE* f_ptr, png_img* image){
+int open_png_read(FILE* f_ptr, png_img* image){
     // Exit if the file is invalid
     if(!check_file(f_ptr)){
         return 0;
     }
-    png_structp png_ptr;
-    png_infop info_ptr;
-    // Exit if setting up structs failed
-    if(!setup_png(&png_ptr, &info_ptr)){
+    // Update the png_ptr pointer to the new struct
+    // Meaning you need to dereference the ptr variable and not the ptr itself
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL,
+      NULL);
+
+    // If there's no pointer then there's an issue
+    if (!png_ptr){
+        perror("Could not initialize PNG write struct \n");
         return 0;
     }
-    // use setjmp since libpng relies on it for errors
-    if (setjmp(png_jmpbuf(png_ptr))){
-        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
-        printf("Encountered an error while reading the png\n");
+  
+    // Same applies here with the info struct
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr) {
+        png_destroy_read_struct(png_ptr, NULL, NULL);
+        perror("Could not initialize PNG info struct \n");
         return 0;
     }
 
-    read_png(png_ptr, info_ptr, f_ptr, image);
+    // use setjmp since libpng relies on it for errors
+    if (setjmp(png_jmpbuf(png_ptr))){
+        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+        perror("Encountered an error while reading the png\n");
+        return 0;
+    }
+    // According to the guide, it takes the file pointer and stores it in the png pointer
+    png_init_io(png_ptr, f_ptr);
+    // Now you can read
+    read_png(png_ptr, info_ptr, image);
+
     // dispose structs
     png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+    return 1;
+}
+
+int write_png(png_structp png_ptr, png_infop info_ptr, png_img* image){
+    int width = image->width;
+    int height = image->height;
+    // Write to the IHDR block
+    png_set_IHDR(png_ptr, info_ptr, width, height,
+        8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE,
+        PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+
+    // Initialize the buffer for storing a row
+    png_bytepp row_pointers = png_malloc(png_ptr, height*(sizeof (png_bytep)));
+    for (int y=0; y<height; y++){
+        // I think you allocate first
+        row_pointers[y] = png_malloc(png_ptr, width*sizeof(png_bytep)*4);
+        // Then write to the rows
+        png_pix* img_row = image->pixels[y];
+        png_bytep wr_row = row_pointers[y];
+
+        for(int x=0; x<width; x++){
+            png_pix pixel = img_row[x];
+            png_bytep pix_address = &(wr_row[x*4]);
+            pix_address[0] = pixel.r;
+            pix_address[1] = pixel.g;
+            pix_address[2] = pixel.b;
+            pix_address[3] = pixel.a;
+        }
+    }
+
+    // Set rows
+    png_set_rows(png_ptr, info_ptr, row_pointers);
+
+    // Write the png to the file
+    // No transforms
+    png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
+}
+
+int open_png_write(FILE* f_ptr, png_img* image){
+    // Set up png_ptr for write
+    png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png_ptr){
+        perror("Could not initialize PNG write struct \n");
+        return 0;
+    }
+
+    // Setup info ptr
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+    if (!info_ptr){
+        png_destroy_write_struct(png_ptr, NULL);
+        perror("Could not initialize PNG info struct \n");
+        return 0;
+    }
+
+    //Setup error handling
+    if(setjmp(png_jmpbuf(png_ptr))){
+        png_destroy_write_struct(&png_ptr, &info_ptr);
+        perror("Encountered an error while writing the png\n");
+        return 0;
+    }
+
+    // Initialize I/O
+    png_init_io(png_ptr, f_ptr);
+    // Commence writing
+    write_png(png_ptr, info_ptr, image);
+        
     return 1;
 }
 
@@ -172,16 +195,33 @@ int load_png(char* path, png_img* image){
 
     // pointer exists if file exists
     if(f_ptr){
-        if(!open_png(f_ptr, image)){
+        if(!open_png_read(f_ptr, image)){
             printf("Failed to read png.\n");
+            fclose(f_ptr);
+            abort();
         }
         // close at the end
         fclose(f_ptr);
         return 1;
-    }else{
-        printf("No such file: %s \n", path);
-        return 0;
     }
 
-    return 1;
+    printf("No such file: %s \n", path);
+    return 0;
+}
+
+int save_png(char* path, png_img* image){
+    FILE* f_ptr = fopen(path, "wb");
+
+    if(f_ptr){
+        if(!open_png_write(f_ptr, image)){
+            printf("Failed to write png.\n");
+            fclose(f_ptr);
+            abort();
+        }
+        fclose(f_ptr);
+        return 1;
+    }
+
+    printf("Could not open stream for writing to %s. \n", path);
+    return 0;
 }
